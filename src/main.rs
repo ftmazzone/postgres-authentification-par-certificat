bd83981183226a661d1367c0348114fd1f20bd42
Florent Mazzone <ftmazzone@yahoo.fr>
2021-05-31 21:19:48 +0200

    Séparer les fonctions

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,16 @@
 use native_tls::{Certificate, Identity, TlsConnector};
 use postgres_native_tls::MakeTlsConnector;
-use serde::{Deserialize, Serialize};
 use simple_signal::{self, Signal};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use std::{fs, time};
 use tokio::time::{sleep, timeout};
 use tokio_postgres::{AsyncMessage, Config, Error};
-
+use std::{fs,time};
+use configuration_bdd::{lire_configuration,ConfigurationBdd};
 use futures::channel::mpsc;
 use futures::{join, stream, FutureExt, StreamExt, TryStreamExt};
 
-#[derive(Deserialize, Serialize)]
-struct ConfigurationBdd {
-    adresse: String,
-    port: u16,
-    utilisateur: String,
-    #[serde(rename = "motDePasse")]
-    mot_de_passe: String,
-    #[serde(rename = "baseDeDonnees")]
-    base_de_donnees: String,
-    application: String,
-    #[serde(rename = "certificatServeur")]
-    certificat_serveur: String,
-    #[serde(rename = "certificatClient")]
-    certificat_client: String,
-    #[serde(rename = "motDePasseCertificatClient")]
-    mot_de_passe_certificat_client: String,
-}
-
-impl Default for ConfigurationBdd {
-    fn default() -> ConfigurationBdd {
-        ConfigurationBdd {
-            adresse: "localhost".to_string(),
-            port: 5432,
-            utilisateur: "postgres".to_string(),
-            mot_de_passe: "******".to_string(),
-            base_de_donnees: "postgres".to_string(),
-            application: "mon_application".to_string(),
-            certificat_serveur: "./certificats/serveur.crt".to_string(),
-            certificat_client: "./certificats/client.pfx".to_string(),
-            mot_de_passe_certificat_client: "******".to_string(),
-        }
-    }
-}
+mod configuration_bdd;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -175,47 +142,4 @@ async fn lire_notifications(
     Ok(())
 }
 
-fn lire_configuration() -> ConfigurationBdd {
-    let mut configuration_bdd: ConfigurationBdd = Default::default();
-    let configuration;
-    match fs::read_to_string("configuration.json") {
-        Ok(c) => configuration = c,
-        Err(e) => {
-            if e.kind() == std::io::ErrorKind::NotFound {
-                let fichier_configuration_texte =
-                    serde_json::to_string_pretty(&configuration_bdd).unwrap();
-                match fs::write("configuration.json", fichier_configuration_texte) {
-                    Ok(m) => {
-                        println!(
-                            "Fichier configuration exemple créé : 'configuration.json' : {:#?}",
-                            m
-                        );
 
-                        configuration = serde_json::to_string(&configuration_bdd).unwrap();
-                    }
-
-                    Err(e) => panic!(
-                        "Erreur lors de la lecture du fichier de configuration : {:#?}",
-                        e
-                    ),
-                };
-            } else {
-                panic!(
-                    "Erreur lors de la lecture du fichier de configuration : {:#?}",
-                    e
-                );
-            }
-        }
-    }
-
-    match serde_json::from_str::<ConfigurationBdd>(&configuration) {
-        Ok(m) => configuration_bdd = m,
-        Err(e) => {
-            panic!(
-                "Erreur lors de la désérialisation du fichier de configuration : {:#?}",
-                e
-            )
-        }
-    }
-    configuration_bdd
-}
