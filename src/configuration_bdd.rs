a5c1e7f898be817593ce3ce07e36f5b5be3fc079
Florent Mazzone <ftmazzone@yahoo.fr>
2021-05-31 21:49:17 +0200

    Recevoir les signals (#2)
    * Afficher les résultats uniquement si aucune erreur n'est détectée
    
    * Ecouter les signaux "INT"
    
    * Séparer les fonctions
    
    * Regrouper les méthodes de base de données dans "gestion_bdd"
    
    * ajouter une action
    
    * modifer l'action

---
+++ src/configuration_bdd.rs
@@ -0,0 +1,81 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Deserialize, Serialize)]
+pub struct ConfigurationBdd {
+    pub adresse: String,
+    pub port: u16,
+    pub utilisateur: String,
+    #[serde(rename = "motDePasse")]
+    pub mot_de_passe: String,
+    #[serde(rename = "baseDeDonnees")]
+    pub base_de_donnees: String,
+    pub application: String,
+    #[serde(rename = "certificatServeur")]
+    pub certificat_serveur: String,
+    #[serde(rename = "certificatClient")]
+    pub certificat_client: String,
+    #[serde(rename = "motDePasseCertificatClient")]
+    pub mot_de_passe_certificat_client: String,
+}
+
+impl Default for ConfigurationBdd {
+    fn default() -> ConfigurationBdd {
+        ConfigurationBdd {
+            adresse: "localhost".to_string(),
+            port: 5432,
+            utilisateur: "postgres".to_string(),
+            mot_de_passe: "******".to_string(),
+            base_de_donnees: "postgres".to_string(),
+            application: "mon_application".to_string(),
+            certificat_serveur: "./certificats/serveur.crt".to_string(),
+            certificat_client: "./certificats/client.pfx".to_string(),
+            mot_de_passe_certificat_client: "******".to_string(),
+        }
+    }
+}
+
+pub fn lire_configuration() -> ConfigurationBdd {
+    let mut configuration_bdd: ConfigurationBdd = Default::default();
+    let configuration;
+    match fs::read_to_string("configuration.json") {
+        Ok(c) => configuration = c,
+        Err(e) => {
+            if e.kind() == std::io::ErrorKind::NotFound {
+                let fichier_configuration_texte =
+                    serde_json::to_string_pretty(&configuration_bdd).unwrap();
+                match fs::write("configuration.json", fichier_configuration_texte) {
+                    Ok(m) => {
+                        println!(
+                            "Fichier configuration exemple créé : 'configuration.json' : {:#?}",
+                            m
+                        );
+
+                        configuration = serde_json::to_string(&configuration_bdd).unwrap();
+                    }
+
+                    Err(e) => panic!(
+                        "Erreur lors de la lecture du fichier de configuration : {:#?}",
+                        e
+                    ),
+                };
+            } else {
+                panic!(
+                    "Erreur lors de la lecture du fichier de configuration : {:#?}",
+                    e
+                );
+            }
+        }
+    }
+
+    match serde_json::from_str::<ConfigurationBdd>(&configuration) {
+        Ok(m) => configuration_bdd = m,
+        Err(e) => {
+            panic!(
+                "Erreur lors de la désérialisation du fichier de configuration : {:#?}",
+                e
+            )
+        }
+    }
+    configuration_bdd
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,25 @@
 use native_tls::{Certificate, Identity, TlsConnector};
 use postgres_native_tls::MakeTlsConnector;
-use serde::{Deserialize, Serialize};
-use std::{fs, time};
-use tokio::time::{sleep, timeout};
-use tokio_postgres::{AsyncMessage, Config, Error};
+use simple_signal::{self, Signal};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use tokio_postgres::{Error};
+use std::{fs};
+use configuration_bdd::{lire_configuration};
+use gestion_bdd::{demarrer_lecture_notifications};
 
-use futures::channel::mpsc;
-use futures::{join, stream, FutureExt, StreamExt, TryStreamExt};
-
-#[derive(Deserialize, Serialize)]
-struct ConfigurationBdd {
-    adresse: String,
-    port: u16,
-    utilisateur: String,
-    #[serde(rename = "motDePasse")]
-    mot_de_passe: String,
-    #[serde(rename = "baseDeDonnees")]
-    base_de_donnees: String,
-    application: String,
-    #[serde(rename = "certificatServeur")]
-    certificat_serveur: String,
-    #[serde(rename = "certificatClient")]
-    certificat_client: String,
-    #[serde(rename = "motDePasseCertificatClient")]
-    mot_de_passe_certificat_client: String,
-}
-
-impl Default for ConfigurationBdd {
-    fn default() -> ConfigurationBdd {
-        ConfigurationBdd {
-            adresse: "localhost".to_string(),
-            port: 5432,
-            utilisateur: "postgres".to_string(),
-            mot_de_passe: "******".to_string(),
-            base_de_donnees: "postgres".to_string(),
-            application: "mon_application".to_string(),
-            certificat_serveur: "./certificats/serveur.crt".to_string(),
-            certificat_client: "./certificats/client.pfx".to_string(),
-            mot_de_passe_certificat_client: "******".to_string(),
-        }
-    }
-}
+mod configuration_bdd;
+mod gestion_bdd;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let operationnel = Arc::new(AtomicBool::new(true));
+    let r = operationnel.clone();
+    simple_signal::set_handler(&[Signal::Int, Signal::Term], move |signal_recu| {
+        println!("Signal reçu : '{:?}'", signal_recu);
+        r.store(false, Ordering::SeqCst);
+    });
+
     lire_configuration();
     let configuration_bdd = lire_configuration();
 
@@ -66,144 +42,10 @@ async fn main() -> Result<(), Error> {
         .unwrap();
 
     let connector = MakeTlsConnector::new(connector);
-    demarrer_lecture_notifications(configuration_bdd, connector).await?;
+    demarrer_lecture_notifications(&operationnel, configuration_bdd, connector).await?;
 
     Ok(())
 }
 
-async fn demarrer_lecture_notifications(
-    configuration_bdd: ConfigurationBdd,
-    connector: postgres_native_tls::MakeTlsConnector,
-) -> Result<(), Error> {
-    let (client, mut connection) = Config::new()
-        .host(&configuration_bdd.adresse)
-        .port(configuration_bdd.port)
-        .user(&configuration_bdd.utilisateur)
-        .dbname(&configuration_bdd.base_de_donnees)
-        .application_name(&configuration_bdd.application)
-        .connect(connector)
-        .await?;
 
-    //Utilisant l'exemple de la page : https://github.com/sfackler/rust-postgres/blob/fc10985f9fdf0903893109bc951fb5891539bf97/tokio-postgres/tests/test/main.rs#L612
-    let (tx, mut rx) = mpsc::unbounded();
-    let stream =
-        stream::poll_fn(move |cx| connection.poll_message(cx)).map_err(|e| panic!("{}", e));
-    let connection = stream.forward(tx).map(|r| r.unwrap());
-    tokio::spawn(connection);
 
-    client
-        .batch_execute(
-            "LISTEN mes_notifications;
-         NOTIFY mes_notifications, 'ma_premiere_notification';
-         NOTIFY mes_notifications, 'ma_seconde_notification';",
-        )
-        .await
-        .unwrap();
-
-    let mut mes_notifications = Vec::<tokio_postgres::Notification>::new();
-
-    let lire_version_resultat = lire_version(&client);
-    let lire_notifications_resultat = lire_notifications(&mut rx, &mut mes_notifications);
-    join!(lire_version_resultat, lire_notifications_resultat);
-    println!(
-        "Etape 1 : notification : {} {:?}",
-        &mut mes_notifications.len(),
-        &mut rx
-    );
-
-    mes_notifications = Vec::<tokio_postgres::Notification>::new();
-    let lire_version_resultat = lire_version(&client);
-    let lire_notifications_resultat = lire_notifications(&mut rx, &mut mes_notifications);
-    join!(lire_version_resultat, lire_notifications_resultat);
-    println!(
-        "Etape 2 : notification : {} {:?}",
-        &mut mes_notifications.len(),
-        &mut rx
-    );
-    rx.close();
-
-    Ok(())
-}
-
-async fn lire_version(client: &tokio_postgres::Client) -> Result<(), Error> {
-    let lignes = client
-        .query("SELECT $1::TEXT || version()", &[&"Connecté à "])
-        .await?;
-
-    let version: &str = lignes[0].get(0);
-    println!("{}", version);
-    sleep(time::Duration::from_secs(5)).await;
-
-    Ok(())
-}
-
-async fn lire_notifications(
-    rx: &mut mpsc::UnboundedReceiver<AsyncMessage>,
-    mes_notifications: &mut Vec<tokio_postgres::Notification>,
-) -> Result<(), Error> {
-    let mut executer = true;
-    let mut attentes = 0;
-    while executer {
-        if let Ok(Some(m)) = timeout(time::Duration::from_secs(5), rx.next()).await {
-            println!("{:?}", m);
-            match m {
-                AsyncMessage::Notification(n) => {
-                    mes_notifications.push(n);
-                    println!("Pour continuer envoyer manuellement une notification.");
-                }
-                _ => {}
-            }
-        } else {
-            println!("Delai d'attente dépassé");
-            attentes += 1;
-        }
-
-        executer = mes_notifications.len() <= 2 && attentes < 5;
-    }
-    Ok(())
-}
-
-fn lire_configuration() -> ConfigurationBdd {
-    let mut configuration_bdd: ConfigurationBdd = Default::default();
-    let configuration;
-    match fs::read_to_string("configuration.json") {
-        Ok(c) => configuration = c,
-        Err(e) => {
-            if e.kind() == std::io::ErrorKind::NotFound {
-                let fichier_configuration_texte =
-                    serde_json::to_string_pretty(&configuration_bdd).unwrap();
-                match fs::write("configuration.json", fichier_configuration_texte) {
-                    Ok(m) => {
-                        println!(
-                            "Fichier configuration exemple créé : 'configuration.json' : {:#?}",
-                            m
-                        );
-
-                        configuration = serde_json::to_string(&configuration_bdd).unwrap();
-                    }
-
-                    Err(e) => panic!(
-                        "Erreur lors de la lecture du fichier de configuration : {:#?}",
-                        e
-                    ),
-                };
-            } else {
-                panic!(
-                    "Erreur lors de la lecture du fichier de configuration : {:#?}",
-                    e
-                );
-            }
-        }
-    }
-
-    match serde_json::from_str::<ConfigurationBdd>(&configuration) {
-        Ok(m) => configuration_bdd = m,
-        Err(e) => {
-            panic!(
-                "Erreur lors de la désérialisation du fichier de configuration : {:#?}",
-                e
-            )
-        }
-    }
-    configuration_bdd
-}
