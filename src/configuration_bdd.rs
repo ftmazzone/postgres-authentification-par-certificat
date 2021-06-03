7d0887136b6d600f4c81fbc1cac337ce74b79a90
Florent Mazzone <ftmazzone@yahoo.fr>
2021-06-03 21:00:20 +0200

    Automatiser la recherche du nom de programme

--- src/configuration_bdd.rs
+++ src/configuration_bdd.rs
@@ -21,13 +21,20 @@ pub struct ConfigurationBdd {
 
 impl Default for ConfigurationBdd {
     fn default() -> ConfigurationBdd {
+        let nom_programme = std::env::current_exe()
+            .expect("L'adresse du programme ne peut être obtenue.")
+            .file_name()
+            .expect("Le nom du programme ne peut être obtenue.")
+            .to_string_lossy()
+            .into_owned();
+
         ConfigurationBdd {
             adresse: "localhost".to_string(),
             port: 5432,
             utilisateur: "postgres".to_string(),
             mot_de_passe: Some("******".to_string()),
             base_de_donnees: "postgres".to_string(),
-            application: "mon_application".to_string(),
+            application: nom_programme.to_string(),
             certificat_serveur: "./certificats/serveur.crt".to_string(),
             certificat_client: Some("./certificats/client.pfx".to_string()),
             mot_de_passe_certificat_client: Some("******".to_string()),
@@ -40,8 +47,8 @@ pub fn lire_configuration() -> ConfigurationBdd {
     let configuration;
     match fs::read_to_string("configuration.json") {
         Ok(c) => configuration = c,
-        Err(e) => {
-            if e.kind() == std::io::ErrorKind::NotFound {
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => {
                 let fichier_configuration_texte =
                     serde_json::to_string_pretty(&configuration_bdd).unwrap();
                 match fs::write("configuration.json", fichier_configuration_texte) {
@@ -53,29 +60,25 @@ pub fn lire_configuration() -> ConfigurationBdd {
 
                         configuration = serde_json::to_string(&configuration_bdd).unwrap();
                     }
-
                     Err(e) => panic!(
                         "Erreur lors de la lecture du fichier de configuration : {:#?}",
                         e
                     ),
-                };
-            } else {
-                panic!(
-                    "Erreur lors de la lecture du fichier de configuration : {:#?}",
-                    e
-                );
+                }
             }
-        }
+            _ => panic!(
+                "Erreur lors de la lecture du fichier de configuration : {:#?}",
+                e
+            ),
+        },
     }
 
     match serde_json::from_str::<ConfigurationBdd>(&configuration) {
         Ok(m) => configuration_bdd = m,
-        Err(e) => {
-            panic!(
-                "Erreur lors de la désérialisation du fichier de configuration : {:#?}",
-                e
-            )
-        }
+        Err(e) => panic!(
+            "Erreur lors de la désérialisation du fichier de configuration : {:#?}",
+            e
+        ),
     }
     configuration_bdd
 }
